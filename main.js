725d02908c86b09610cc5ab0b6f17e0310392695
Shiva Goud <bshivagoud@gmail.com>
2017-03-22 00:07:07 +0530

    fetched all the data from the server and properly restructured

--- main.js
+++ main.js
@@ -4,21 +4,40 @@ app.controller('chartCtrl',function($scope,$http,$sce) {
     $scope.message= "Hello world";
 	$scope.populationData = ["Apple","Orange","Banana"];
 	$scope.refresh = function(){
-		var url = "http://climatedataapi.worldbank.org/climateweb/rest/v1/country/mavg/tas/1980/1999/IND.json?callback=mycallback";
-		$sce.trustAsResourceUrl(url);
+		var indicators = ["SP.POP.TOTL","NY.GDP.PCAP.CD","SP.DYN.LE00.IN"];
+		var countries = {};
+		var loaded  = false;
+		var url_prefix  = "http://api.worldbank.org/countries/all/indicators/";
+		$sce.trustAsResourceUrl(url_prefix);
 		
 		//global variable
-		mycallback = function(res){
+		callback_success=[false,false,false];
+		get_population_data = function(res){
 			console.log("success");
-			$scope.populationData = res;
+			res[1].forEach(function(entry,index) {
+				var country_id = entry["country"]["id"];
+				var indicator_id = entry["indicator"]["id"];
+				var value = entry["value"];
+				if(countries[country_id] === undefined)
+					countries[country_id] = {};
+				countries[country_id][indicator_id] = value;
+				
+				if(index==0){
+					for(i=0;i<3;i++)
+						if(indicator_id==indicators[i])
+							callback_success[i] = true;
+				}
+			});
+			if(callback_success[0]&&callback_success[1]&&callback_success[2]){
+				console.log(countries);
+				$scope.populationData = countries;
+			}
 		}
 		
-		$http.jsonp(url)
-		.then(function(response) {
-			console.log(response);
-		},function(error){
-			console.log("Error");
-			console.log(error);
+		indicators.forEach(function(ind){
+			var url_end = "?date=2014:2014&per_page=500&format=jsonP&prefix=get_population_data";
+			$http.jsonp(url_prefix + ind + url_end);
 		});
+		
 	}
 });
