83d02ef03bca33ce206dc598df4a5f78187299bd
Shiva Goud <bshivagoud@gmail.com>
2017-03-21 17:49:40 +0530

    fetching data from the server

--- main.js
+++ main.js
@@ -1,5 +1,19 @@
 var app = angular.module('populationMonitorApp', []);
 
-app.controller('chartCtrl',function($scope) {
+app.controller('chartCtrl',function($scope,$http) {
     $scope.message= "Hello world";
+	$scope.populationData = ["Apple","Orage","Banana"];
+	$scope.refresh = function(){
+		//test with https://www.w3schools.com/angular/welcome.htm
+		$http({
+			method : "GET",
+			url : $scope.url
+		}).then(function(response) {
+			console.log(response);
+			$scope.populationData = response.data;
+		},function(error){
+			console.log("Error");
+			console.log(error);
+		});
+	}
 });
